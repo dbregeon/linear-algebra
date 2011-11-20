7778314a5ff1dc34bef6527badcd532ce72bf4a9
Denis Bregeon <denis.bregeon@digitalbrikes.com>
2011-11-19 21:03:11 -0800

    Introduced packages

---
+++ src/main/scala/com/digitalbrikes/machine_learning/GradientDescent.scala
@@ -0,0 +1,22 @@
+package com.digitalbrikes.machine_learning
+
+import com.digitalbrikes.linear_algebra.Nat._
+import com.digitalbrikes.linear_algebra.Vector._
+
+/**
+ * alpha is the learning  rate of the gradient descent.
+ * iterations is the number of steps in the descent.
+ */
+class GradientDescent(val alpha : BigDecimal, val iterations : Int) extends Serializable {
+	def apply[N  <: Nat](gradient : (Vector[N] => Vector[N]), theta0 : Vector[N]) : Vector[N] = {
+		var result = theta0;
+		for (iteration <- 0 until iterations) {
+			result = result - alpha *: gradient(result)
+		}
+		result
+	}
+}
+
+object GradientDescent {
+	def apply(alpha : BigDecimal, iterations : Int) : GradientDescent = new GradientDescent(alpha, iterations)
+}
---
+++ src/test/scala/com/digitalbrikes/linear_algebra/MatrixTest.scala
@@ -0,0 +1,60 @@
+package com.digitalbrikes.linear_algebra
+
+import org.scalacheck._
+import org.scalacheck.Prop._
+import Nat._
+
+object MatrixSpecification extends Properties("Matrix") {
+	import Arbitrary.arbitrary
+	
+	val unlimitedBigDecimalGen = for {
+		  n <- arbitrary[Double]
+	} yield (BigDecimal(n, java.math.MathContext.UNLIMITED))
+
+	val sameDimensionMatrixGenerator = for {
+		  n <- Gen.choose(1, 100)
+		  m <- Gen.choose(1, 100)
+		  values1 <- Gen.containerOfN[Array, BigDecimal](n * m, unlimitedBigDecimalGen)
+		  values2 <- Gen.containerOfN[Array, BigDecimal](n * m, unlimitedBigDecimalGen)
+	} yield (Matrix(Nat(n), Nat(m), values1), Matrix(Nat(n), Nat(m), values2))
+	
+	val matrixGenerator = for {
+		  n <- Gen.choose(1, 100)
+		  m <- Gen.choose(1, 100)
+		  values1 <- Gen.containerOfN[Array, BigDecimal](n * m, unlimitedBigDecimalGen)
+	} yield (Matrix(Nat(n), Nat(m), values1))
+	
+	def addMatrices[M <: Nat, N <: Nat](tuple: (Matrix[M, N], Matrix[M, N])) = {
+		val (a, b) =  tuple
+		(a + b) == (b + a)
+	}
+	
+	def subtractMatrices[M <: Nat, N <: Nat](tuple: (Matrix[M, N], Matrix[M, N])) = {
+		val (a, b) =  tuple
+		b == (a + b - a)
+	}
+
+	property("addition is commutative") = forAll(sameDimensionMatrixGenerator) {tuple  => 
+		addMatrices(tuple)
+	}
+	
+	property("subtraction is the inverse of the addition") = forAll(sameDimensionMatrixGenerator)  {tuple =>
+	  	subtractMatrices(tuple)
+	}
+	
+	property("scalar multiplication is associative") = forAll(matrixGenerator, unlimitedBigDecimalGen, unlimitedBigDecimalGen) {(m : Matrix[_, _], s1 : BigDecimal, s2 : BigDecimal) => 
+		((s1 * s2) *: m) == (s1 *: (s2 *: m))
+	}
+	
+	property("scalar multiplication is associative") = forAll(matrixGenerator, unlimitedBigDecimalGen, unlimitedBigDecimalGen) {(m : Matrix[_, _], s1 : BigDecimal, s2 : BigDecimal) => 
+		((s1 * s2) *: m) == (s1 *: (s2 *: m))
+	}
+	
+	property("one is the neutral element of the scalar multiplication") = forAll(matrixGenerator) {(m : Matrix[_, _]) => 
+		(1 *: m) == m
+	}
+	
+	property("transpose of transpose is the same matrix") = forAll(matrixGenerator) {(m : Matrix[_, _]) => 
+		(m.transpose).transpose == m
+	}
+}
